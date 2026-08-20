0b8324f63173aa2f488ec4e54b4cbf6fa84998e8
Diego <dmchamendy@uamv.edu.ni>
2026-08-20 15:16:59 -0600

    Commit ejemplo 5

---
+++ src/main/java/ni/edu/ni/ejemplo5/HelloController.java
@@ -0,0 +1,14 @@
+package ni.edu.ni.ejemplo5;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
